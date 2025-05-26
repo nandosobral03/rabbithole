9d9560ea05aac82eb660a30b28c7e9a5374173dc
nandosobral03 <nandosobral03@gmail.com>
2025-05-26 08:57:27 -0300

    display swap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,37 +8,30 @@ import { TRPCReactProvider } from "~/trpc/react";
 import { ThemeProvider } from "./_components/providers/theme-provider";
 
 export const metadata: Metadata = {
-	title: "rabbithole",
-	description: "Explore Wikipedia rabbit holes and share them with the world",
-	icons: [{ rel: "icon", url: "/favicon.png" }],
+  title: "rabbithole",
+  description: "Explore Wikipedia rabbit holes and share them with the world",
+  icons: [{ rel: "icon", url: "/favicon.png" }],
 };
 
 const geist = Geist({
-	subsets: ["latin"],
-	variable: "--font-geist-sans",
+  subsets: ["latin"],
+  variable: "--font-geist-sans",
+  display: "swap",
 });
 
-export default async function RootLayout({
-	children,
-}: Readonly<{ children: React.ReactNode }>) {
-	const theme = await getThemeFromCookies();
+export default async function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  const theme = await getThemeFromCookies();
 
-	return (
-		<html
-			lang="en"
-			className={`${geist.variable} ${theme === "system" ? "dark" : theme}`}
-		>
-			<head>
-				<link
-					href="https://api.fontshare.com/v2/css?f[]=chillax@200,300,400,500,600,700,1&display=swap"
-					rel="stylesheet"
-				/>
-			</head>
-			<body>
-				<ThemeProvider defaultTheme={theme}>
-					<TRPCReactProvider>{children}</TRPCReactProvider>
-				</ThemeProvider>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en" className={`${geist.variable} ${theme === "system" ? "dark" : theme}`}>
+      <head>
+        <link href="https://api.fontshare.com/v2/css?f[]=chillax@200,300,400,500,600,700,1&display=swap" rel="stylesheet" />
+      </head>
+      <body>
+        <ThemeProvider defaultTheme={theme}>
+          <TRPCReactProvider>{children}</TRPCReactProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
 }
